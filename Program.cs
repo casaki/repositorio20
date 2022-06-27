03e201a3e5b38b2e163fed7d2513cc30789e0c2e
Carlos sanchez <carlos.sanchez@codehouse.academy>
2022-06-27 12:10:44 +0200

    Program.cs cambiado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-Console.Write("Buenos días, Como te llamas?");
+Console.Write("Buenos noches, Como te llamas?");
 var name= Console.ReadLine();
 Console.Write($"Hola, {name}, te gusta programar?")
 var respuesta = Console.ReadLine();
